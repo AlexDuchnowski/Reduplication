29192e06c64eb39b9aee688cc1bc97c9845cbab9
AlexDuchnowski <alex_duchnowski@brown.edu>
2024-04-07 17:19:38 -0400

    extracting reduplicated content alongside original context

--- scripts/extract_redups.sh
+++ scripts/extract_redups.sh
@@ -1,5 +1,5 @@
 # NOTE: Replace the path to the data file with the correct path
 
-cat $1 | sed -re 's/[[(].*[])]//g' | sed -re 's/--/-/g' | sed -e 's/\(.*\)/\L\1/' | egrep '\b([a-z]+ ?[a-z]*)[ -]\1\b' | sed -re 's/.*\b([a-z]+ ?[a-z]*)[ -]\1\b.*/\1/g'
+cat $1 | sed -re 's/[[(].*[])]//g' | sed -re 's/--/-/g' | sed -e 's/\(.*\)/\L\1/' | egrep '\b([a-z]+ ?[a-z]*)[ -]\1\b' | sed -re 's/(.*\b)([a-z]+ ?[a-z]*)([ -])\2(\b.*)/\2\t\1\2\3\2\4/g'
 
 # OLD REGEX: egrep -i '\b(.*\w+.*),?[ -]+\1\b'
