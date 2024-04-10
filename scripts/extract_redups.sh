1764272ddf95df6761811965929cf0af689f16fc
AlexDuchnowski <alex_duchnowski@brown.edu>
2024-04-10 18:17:01 -0400

    some example filtering files and a file with ideas for filtering further

--- scripts/extract_redups.sh
+++ scripts/extract_redups.sh
@@ -1,5 +1,7 @@
 # NOTE: Replace the path to the data file with the correct path
 
-cat $1 | sed -re 's/[[(].*[])]//g' | sed -re 's/--/-/g' | sed -e 's/\(.*\)/\L\1/' | egrep '\b([a-z]+ ?[a-z]*)[ -]\1\b' | sed -re 's/(.*\b)([a-z]+ ?[a-z]*)([ -])\2(\b.*)/\2\t\1\2\3\2\4/g'
+# Remove bracketed content, convert to lowercase, find reduplications, and output in tab-separated format
+cat $1 | sed -re 's/[[(].*[])]//g' | sed -e 's/\(.*\)/\L\1/' | egrep '\b([a-z]+ ?[a-z]*)[ -]\1\b' | sed -re 's/(.*\b)([a-z]+ ?[a-z]*)([ -])\2(\b.*)/\2\t\1\2\3\2\4/g'
 
 # OLD REGEX: egrep -i '\b(.*\w+.*),?[ -]+\1\b'
+# Filtering out (some) double hyphens: sed -re 's/--/-/g'
