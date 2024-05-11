897490b8e640f1e0654bb4e3c3158deb0b354218
AlexDuchnowski <alex_duchnowski@brown.edu>
2024-05-11 15:48:31 -0400

    agreed annotations for opensubs round 1

--- scripts/iaa.py
+++ scripts/iaa.py
@@ -2,6 +2,8 @@ import krippendorff
 import numpy as np
 import pandas as pd
 
+# Salad-Salad Agreement
+
 dfs = [
     pd.read_csv(f"data/annotations/aggregated_salad_{i}.csv", header=None).drop(
         columns=[0]
@@ -11,7 +13,10 @@ dfs = [
 
 rds = [
     np.array(
-        [[np.nan if label in "UN" else label for label in df[i]] for i in range(1, 4)]
+        [
+            [np.nan if label in "UN" else label.upper() for label in df[i]]
+            for i in range(1, 4)
+        ]
     )
     for df in dfs
 ]
@@ -21,3 +26,14 @@ for i in range(2):
         f"Krippendorff's alpha for phase {i+1}:",
         krippendorff.alpha(reliability_data=rds[i], level_of_measurement="nominal"),
     )
+
+# OpenSubtitles Agreement
+
+df = pd.read_csv("data/annotations/aggregated_opensubs_1.csv", header=None).drop(
+    columns=[0]
+)
+rd = np.array([[label.upper() for label in df[i]] for i in range(1, 4)])
+print(
+    "Krippendorff's alpha for OpenSubtitles:",
+    krippendorff.alpha(reliability_data=rd, level_of_measurement="nominal"),
+)
