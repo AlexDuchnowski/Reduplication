f55309e581824ff234acb36dda4798d16ce8b924
AlexDuchnowski <alex_duchnowski@brown.edu>
2024-05-15 21:08:20 -0400

    writeup changes

--- scripts/iaa.py
+++ scripts/iaa.py
@@ -23,17 +23,25 @@ rds = [
 
 for i in range(2):
     print(
-        f"Krippendorff's alpha for phase {i+1}:",
+        f"Krippendorff's alpha for Salad-Salad {i+1}:",
         krippendorff.alpha(reliability_data=rds[i], level_of_measurement="nominal"),
     )
 
 # OpenSubtitles Agreement
 
-df = pd.read_csv("data/annotations/aggregated_opensubs_1.csv", header=None).drop(
-    columns=[0]
-)
-rd = np.array([[label.upper() for label in df[i]] for i in range(1, 4)])
-print(
-    "Krippendorff's alpha for OpenSubtitles:",
-    krippendorff.alpha(reliability_data=rd, level_of_measurement="nominal"),
-)
+dfs = [
+    pd.read_csv(f"data/annotations/aggregated_opensubs_{i}.csv", header=None).drop(
+        columns=[0]
+    )
+    for i in range(1, 3)
+]
+
+rds = [
+    np.array([[label.upper() for label in df[i]] for i in range(1, 4)]) for df in dfs
+]
+
+for i in range(2):
+    print(
+        f"Krippendorff's alpha for OpenSubs {i+1}:",
+        krippendorff.alpha(reliability_data=rds[i], level_of_measurement="nominal"),
+    )
